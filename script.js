9c1ed2dea9f8df7e16ba9a4dc73e59785600e573
mary-olek <mariany.olekzechen@escola.pr.gov.br>
2025-06-16 11:48:00 +0000

    commmit 2

---
+++ script.js
@@ -0,0 +1,51 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaresultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+    enunciado:"pergunta 1",
+    alternativas:[
+        
+        "alternativa 1",
+        "alternativa 2"
+    ]
+    },     
+    {
+    enunciado:"pergunta 2",
+    alternativas:[
+        
+        "alternativa 1",
+        "alternativa 2"
+    ]
+    },     
+   {
+    enunciado:"pergunta 3",
+    alternativas:[
+        
+        "alternativa 1",
+        "alternativa 2"
+    ]
+    },         
+    {
+    enunciado:"pergunta 4",
+    alternativas:[
+        
+        "alternativa 1",
+        "alternativa 2"
+    ]
+    },     
+    {
+    enunciado:"pergunta 5",
+     alternativas:[
+        
+        "alternativa 1",
+        "alternativa 2"
+    ]
+    },     
+    
+    
+
+]
