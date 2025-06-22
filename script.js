c4fbad9bb9e855b995b83307f521f7a2c7afae84
mary-olek <mariany.olekzechen@escola.pr.gov.br>
2025-06-22 00:44:57 +0000

    commit 3

--- script.js
+++ script.js
@@ -2,50 +2,58 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const caixaresultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-    enunciado:"pergunta 1",
-    alternativas:[
-        
-        "alternativa 1",
-        "alternativa 2"
-    ]
-    },     
+        enunciado: "Escolha seu personagem",
+        alternativas: [
+            "Maya",
+            "Matteo"
+        ]
+    },
     {
-    enunciado:"pergunta 2",
-    alternativas:[
-        
-        "alternativa 1",
-        "alternativa 2"
-    ]
-    },     
-   {
-    enunciado:"pergunta 3",
-    alternativas:[
-        
-        "alternativa 1",
-        "alternativa 2"
-    ]
-    },         
+        enunciado: "",
+        alternativas: [
+            "",
+            "",
+        ]
+    },
     {
-    enunciado:"pergunta 4",
-    alternativas:[
-        
-        "alternativa 1",
-        "alternativa 2"
-    ]
-    },     
+        enunciado: "",
+        alternativas: [
+            "",
+            "",
+        ]
+    },
     {
-    enunciado:"pergunta 5",
-     alternativas:[
-        
-        "alternativa 1",
-        "alternativa 2"
-    ]
-    },     
-    
-    
+        enunciado: "",
+        alternativas: [
+            "",
+            "",
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [
+           "",
+            "",]
+    },
 
-]
+    {
+        enunciado: "",
+        alternativas: [
+           "",
+            "",]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
